43f3f516139c429a8ad7afc434db743a02e8f500
sarakibalogunworks <sarakibalogunworks@gmail.com>
2026-04-13 17:49:05 -0400

    commit

---
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class TheaterReservations {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String fullName = scanner.nextLine().trim();
+
+        System.out.println("What date will you be coming (MM/dd/yyyy: ");
+        String dateInput = scanner.nextLine().trim();
+
+
+    }
+}
